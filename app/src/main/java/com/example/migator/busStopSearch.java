4afd05cf1259bd83a5e710c58f4d58500c4e3d84
Radosław Witkowicz <radoslaw.witkowicz@edu.uekat.pl>
2024-11-13 19:12:53 +0100

    Menu of appliaction added and visual boost

--- app/src/main/java/com/example/migator/busStopSearch.java
+++ app/src/main/java/com/example/migator/busStopSearch.java
@@ -2,27 +2,92 @@ package com.example.migator;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class busStopSearch extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class busStopSearch extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bus_stop_search);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        /*--------------------HOOKS---------------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*--------------------TOOLBAR---------------------*/
+        setSupportActionBar(toolbar);
+
+        /*--------------------NAVIGATION DRAWER MENU---------------------*/
+
+        //hide or show items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_home).setVisible(true); //przykład
+
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_searchBusStop);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if(menuItem.getItemId() == R.id.nav_home) {
+            Intent intent = new Intent(busStopSearch.this, MainActivity.class);
+            startActivity(intent);
+        } else if(menuItem.getItemId() == R.id.nav_searchBusStop) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else if(menuItem.getItemId() == R.id.nav_searchLine) {
+            //Intent intent = new Intent(busStopSearch.this, lineSearch.class);
+            //startActivity(intent);
+            drawerLayout.closeDrawer(GravityCompat.START); //na razie póki nie ma innych ekranów
+        } else if(menuItem.getItemId() == R.id.nav_map) {
+            //Intent intent = new Intent(busStopSearch.this, busStopMap.class);
+            //startActivity(intent);
+            drawerLayout.closeDrawer(GravityCompat.START); //na razie póki nie ma innych ekranów
+        } else if(menuItem.getItemId() == R.id.nav_settings) {
+            //Intent intent = new Intent(busStopSearch.this, settings.class);
+            //startActivity(intent);
+            drawerLayout.closeDrawer(GravityCompat.START); //na razie póki nie ma innych ekranów
+        }
+
+        return true;
     }
 
     public void GoTo_busStopResult(View v){
