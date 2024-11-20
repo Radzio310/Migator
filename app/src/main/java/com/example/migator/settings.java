b7285306ba2867e0871f7ef7d9be7462fe575398
Jakub <jakub.szczepanski02@gmail.com>
2024-11-20 21:53:06 +0100

    Menu for settings

--- app/src/main/java/com/example/migator/settings.java
+++ app/src/main/java/com/example/migator/settings.java
@@ -3,22 +3,89 @@ package com.example.migator;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class settings extends AppCompatActivity{
+import com.google.android.material.navigation.NavigationView;
+
+public class settings extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        /*--------------------HOOKS---------------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*--------------------TOOLBAR---------------------*/
+        setSupportActionBar(toolbar);
+
+        /*--------------------NAVIGATION DRAWER MENU---------------------*/
+
+        //hide or show items
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_home).setVisible(true); //przykład
+
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_settings);
     }
 
 
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if(menuItem.getItemId() == R.id.nav_home) {
+            Intent intent = new Intent(settings.this, MainActivity.class);
+            startActivity(intent);
+
+        } else if(menuItem.getItemId() == R.id.nav_searchBusStop) {
+            Intent intent = new Intent(settings.this, busStopSearch.class);
+            startActivity(intent);
+        } else if(menuItem.getItemId() == R.id.nav_searchLine) {
+            Intent intent = new Intent(settings.this, lineSearch.class);
+            startActivity(intent);
+        } else if(menuItem.getItemId() == R.id.nav_map) {
+            //Intent intent = new Intent(MainActivity.this, busStopMap.class);
+            //startActivity(intent);
+            drawerLayout.closeDrawer(GravityCompat.START); //na razie póki nie ma innych ekranów
+        } else if(menuItem.getItemId() == R.id.nav_settings) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        return true;
+    }
 
 
 
